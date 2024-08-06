61e95fb723fa99d92b6f9cf87437fa8e04136520
yeraldycarpio <2430933@clases.edu.sv>
2024-08-06 17:43:50 -0600

    estructura del proyecto y configuracion de vista layouts

---
+++ src/main/java/org/esfe/Test01JavaeehycmApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01JavaeehycmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test01JavaeehycmApplication.class, args);
+	}
+
+}
